b7d6df66c1206242751bab4e86a367ea0cc6789a
JerryU <jerry.uusitalo@gmail.com>
2024-03-28 20:36:30 +0200

    Create ternary comparison for objects setting using the right database

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const PORT = process.env.PORT
-const MONGODB_STRING = process.env.MONGODB_STRING;
+const PORT = process.env.PORT;
+const MONGODB_STRING =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_STRING
+    : process.env.MONGODB_STRING;
 
 module.exports = {
-    MONGODB_STRING,
-    PORT
-}
+  MONGODB_STRING,
+  PORT,
+};
